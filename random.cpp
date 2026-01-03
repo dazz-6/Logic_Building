c409c5a3c9a96ca2eb2e3fe89f2ec6750d923a63
Dakshi <dakshivashishtha06@yahoo.com>
2026-01-03 23:23:44 +0530

    added new file

---
+++ random.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  cout << " Hello" << endl;
+  cout << "Dakshi" << endl;
+
+  int a = 5;
+  int b =10;
+  cout << a-b <<endl;
+  cout << a+b << endl;
+  cout << b/a;
+  return 0;
+}
