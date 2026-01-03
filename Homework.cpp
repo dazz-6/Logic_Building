b8437c2548b536668ae435906f10d2ba6456247e
Dakshi <dakshivashishtha06@yahoo.com>
2026-01-04 00:26:22 +0530

    added new file

--- Homework.cpp
+++ Homework.cpp
@@ -7,7 +7,7 @@ int main(){
   cin >> a>> b>> c;
   cout << "Product: " << a*b*c << endl;
   int d,e;
-  cout << "Give me another 2 numbers: " << endl;
+  cout << "Give me another 2 nu mbers: " << endl;
   cin >> d >> e;
   cout << "Average of your numbers is : " << (d+e)/2;
 }
---
+++ Homework1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int sum =0;
+  int even = 0;
+
+  //ques: sum of 10 numbers
+  for (int i=0; i<=10 ; i++){
+    sum = sum + i; 
+  }
+   cout << sum << endl;
+
+  //sum of first 10 numbers
+  // for (int i=1;i <= 10; i++){
+  //     even += 2 * i; 
+  // }
+ 
+  // 2nd APPROACH
+  int n =10; 
+   even = n * (n+1);
+  cout << even << endl;
+
+
+  // sum of even numbers between 20 and 40 (both included)
+  // for even numbers in a range, we can use AP formula [ n(a+l)/2]
+  int a= 20, l= 40;
+  int m= ((l - a )/2) +1 ;
+  int even_num = (m * (a+ l)) /2;
+    cout << even_num;
+  
+}
