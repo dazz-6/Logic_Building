c4a306366c8474cb4b45bc8fb652526beedaa3d2
Dakshi <dakshivashishtha06@yahoo.com>
2026-01-03 23:31:30 +0530

    added new file

---
+++ Homework.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a,b,c;
+  cout <<"Give me 3 numbers." << endl;
+  cin >> a>> b>> c;
+  cout << "Product: " << a*b*c << endl;
+  int d,e;
+  cout << "Give me another 2 numbers: " << endl;
+  cin >> d >> e;
+  cout << "Average of your numbers is : " << (d+e)/2;
+}
