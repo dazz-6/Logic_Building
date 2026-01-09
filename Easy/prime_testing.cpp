04682ec6e9a595b73658f0d4f302d111a34c53f1
Dakshi <dakshivashishtha06@yahoo.com>
2026-01-09 19:16:48 +0530

    added

--- Easy/prime_testing.cpp
+++ Easy/prime_testing.cpp
@@ -1,6 +1,19 @@
 //simple method
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-  g
+bool is_prime(int n){
+  // making a loop for 2 to n-1 as prime num are the numbers that are only divisible by themselves and 1
+  for (int i =2;i<n;i++){
+      if(n % i == 0){
+        return false;
+      } 
+    }
+  return true;
 }
+int main(){
+  is_prime(10) ? cout<< "true\n" : cout << "false\n";
+  is_prime(13) ? cout<< "true\n" : cout<<"false\n" ;
+}
+
+// this has time complexity: O(n)
+//and space complexity: O(1)
