b08a19fb78cd69f1a3a8c7714c080c8c2fda12a5
Dakshi <dakshivashishtha06@yahoo.com>
2026-01-09 19:00:30 +0530

    Update

--- Basic/sumofnum.cpp
+++ Basic/sumofnum.cpp
@@ -1,11 +1,28 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int n;
+//   cin >> n;
+//   int sum = 0;
+//   for(int i=1;i<=n;i++){
+//     sum = sum + i;
+//   }
+//   cout << sum;
+// }
+
+
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-  int n;
-  cin >> n;
-  int sum = 0;
-  for(int i=1;i<=n;i++){
-    sum = sum + i;
+int findSum(int n){
+  if (n==0)
+  return 0;
+
+ int result = n*(n+1);
+  return  result/2;
+    
+
   }
-  cout << sum;
+int main(){
+  int num = 3;
+  cout << findSum(num);
 }
---
+++ func.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int double_of_num(int n){\
+  // body
+  int result = 2*n;
+  return result;
+}
+
+int main(){
+int m =10;
+int result = double_of_num(m);
+cout << result <<endl;
+}
